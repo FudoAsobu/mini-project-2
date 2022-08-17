d62b95d0bc074a59d730c78dceb9bba4ca70b5af
121001491_Lê Tấn Phát <99623910+FudoAsobu@users.noreply.github.com>
2022-08-17 14:14:23 +0700

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,35 @@
+function start()
+{
+
+    if (h === null)
+    {
+        h = parseInt(document.getElementById('h_val').value);
+        m = parseInt(document.getElementById('m_val').value);
+        s = parseInt(document.getElementById('s_val').value);
+    }
+ 
+    if (s === -1){
+        m -= 1;
+        s = 59;
+    }
+
+    if (m === -1){
+        h -= 1;
+        m = 59;
+    }
+ 
+    if (h == -1){
+        clearTimeout(timeout);
+        alert('Hết giờ');
+        return false;
+    }
+ 
+    document.getElementById('h').innerText = h.toString();
+    document.getElementById('m').innerText = m.toString();
+    document.getElementById('s').innerText = s.toString();
+ 
+    timeout = setTimeout(function(){
+        s--;
+        start();
+    }, 1000);
+}
